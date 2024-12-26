4f016e4d6b78d3b731cca9ae8f275cda4330248f
divyayelewar <divyayelewar@gmail.com>
2024-12-26 16:55:44 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from './Components/Hero/Hero'
+import Banner from './Components/Carosual/banner'
+import Footer from './Components/Footer/footer'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Hero/>
+      <Banner/>
+      <Footer/>
+      
+      
+    </div>
+  )
+}
+
+export default App
